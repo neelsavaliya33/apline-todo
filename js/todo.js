d931f681a4fb27fcb87048c67f012cb533fa44e4
Neel <neelsavaliya123@gmail.com>
2022-12-23 17:28:51 +0530

    no message

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,19 @@
+window.todoStore = {
+	todos: JSON.parse(localStorage.getItem("todos") || "[]"),
+	save() {
+		localStorage.setItem("todos", JSON.stringify(this.todos));
+	},
+};
+
+window.Todo = function (body) {
+	this.id = Date.now();
+	this.body = body;
+	this.completed = false;
+};
+
 window.todoapp = function () {
 	return {
-		id: 1,
+		...todoStore,
 
 		filter: "all",
 
@@ -8,21 +21,19 @@ window.todoapp = function () {
 
 		editable: 0,
 
-		todos: [],
-
 		addTodo() {
-			this.todos.push({
-				id: this.id,
-				body: this.todoBody,
-				completed: false,
-			});
+			if (!this.todoBody.trim()) {
+				return;
+			}
+			this.todos.push(new Todo(this.todoBody));
+			this.save();
 			this.todoBody = "";
-			this.id++;
 		},
 
 		deleteTodo(todo) {
 			const index = this.todos.indexOf(todo);
 			this.todos.splice(index, 1);
+			this.save();
 		},
 
 		get completed() {
@@ -41,13 +52,24 @@ window.todoapp = function () {
 			}[this.filter];
 		},
 
+		toggleTodo(todo) {
+			todo.completed = !todo.completed;
+			this.save();
+		},
+
+		get isAllCompleted() {
+			return this.todos.length == this.completed.length;
+		},
+
 		clearCompleted() {
 			this.todos = this.active;
+			this.save();
 		},
 
 		editTodo(todo) {
 			todo.cacheBody = todo.body;
 			this.editable = todo.id;
+			this.save();
 		},
 
 		preventEdits(todo) {
@@ -56,12 +78,17 @@ window.todoapp = function () {
 		},
 
 		finishEditing(todo) {
+			if (!todo.body.trim()) {
+				return this.deleteTodo(todo);
+			}
 			delete todo.cacheBody;
 			this.editable = 0;
+			this.save();
 		},
 
-		markAllAsCompleted() {
-			this.todos.map((todo) => (todo.completed = true));
+		toggleAll() {
+			let isCompleted = this.isAllCompleted;
+			this.todos.map((todo) => (todo.completed = !isCompleted));
 		},
 	};
 };
